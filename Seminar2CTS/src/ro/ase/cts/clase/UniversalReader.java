02387338566ca860d652f7f775ad2f4b05555619
Cristi <cristibaban1998@gmail.com>
2021-03-04 12:22:32 +0200

    Am creat readers pentru fiecare clasa

---
+++ Seminar2CTS/src/ro/ase/cts/clase/UniversalReader.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.clase;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class UniversalReader {
+	//punct de legatura catre readere
+	//TO DO !!!
+
+
+
+}
