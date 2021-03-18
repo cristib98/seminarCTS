b5a00a24496da318af277ed09d16d265df2c5416
Cristi <cristibaban1998@gmail.com>
2021-03-18 13:15:57 +0200

    Final commit

--- Seminar4/src/ro/ase/cts/clase/Cabana.java
+++ Seminar4/src/ro/ase/cts/clase/Cabana.java
@@ -1,8 +1,49 @@
 package ro.ase.cts.clase;
 
 public class Cabana {
-	// cABana - Alexandru Baban
-	
-	
+	// CABana - Cristian Alexandru Baban
+
+	private String nume;
+	private int numarCamere;
+	private float pretPeNoapte;
+
+	private static Cabana cabana = null;
+
+	private Cabana(String nume, int numarCamere, float pretPeNoapte) {
+		super();
+		this.nume = nume;
+		this.numarCamere = numarCamere;
+		this.pretPeNoapte = pretPeNoapte;
+	}
+
+	public static synchronized Cabana getInstance(String nume, int numarCamere, float pretPeNoapte) {
+
+		if (cabana == null) {
+			cabana = new Cabana(nume, numarCamere, pretPeNoapte);
+		}
+
+		return cabana;
+	}
+
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+
+	public void setNumarCamere(int numarCamere) {
+		this.numarCamere = numarCamere;
+	}
+
+	public void setPretPeNoapte(float pretPeNoapte) {
+		this.pretPeNoapte = pretPeNoapte;
+	}
+
+	public static void setCabana(Cabana cabana) {
+		Cabana.cabana = cabana;
+	}
+
+	@Override
+	public String toString() {
+		return "Cabana [nume = " + nume + ", numarCamere = " + numarCamere + ", pretPeNoapte = " + pretPeNoapte + " lei]";
+	}
 
 }
--- Seminar4/src/ro/ase/cts/program/Main.java
+++ Seminar4/src/ro/ase/cts/program/Main.java
@@ -1,5 +1,6 @@
 package ro.ase.cts.program;
 
+import ro.ase.cts.clase.Cabana;
 import ro.ase.cts.clase.ManagerSpital;
 import ro.ase.cts.clase.ManagerSpitalLazy;
 
@@ -8,19 +9,27 @@ public class Main {
 	public static void main(String[] args) {
 		ManagerSpital manager1 = ManagerSpital.getInstance();
 		ManagerSpital manager2 = ManagerSpital.getInstance();
-		
+
 		System.out.println(manager1.toString());
-		System.out.println(manager2.toString());	
-		
-		
+		System.out.println(manager2.toString());
+
 		ManagerSpitalLazy managerLazy1 = ManagerSpitalLazy.getInstance("Manager lazy", 33, 2334);
 		ManagerSpitalLazy managerLazy2 = ManagerSpitalLazy.getInstance("Ionescu Mihai", 36, 5133);
-		
-		managerLazy1.setVarsta(44);
-		managerLazy2.setNume("John");
-		
+
+
 		System.out.println(managerLazy1.toString());
 		System.out.println(managerLazy2.toString());
+	
+		System.out.println("\n------------------------------------------------------\n");
+		
+		Cabana cabana1 = Cabana.getInstance("Busteni", 5, 70.5f);
+		Cabana cabana2 = Cabana.getInstance("Cerbul", 6, 90f);
+		
+		cabana1.setNumarCamere(10);
+
+		System.out.println(cabana1.toString());
+		System.out.println(cabana2.toString());
+
 	}
 
 }
